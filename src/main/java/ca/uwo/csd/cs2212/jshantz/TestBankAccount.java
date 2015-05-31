75b32a58c3b5691319c57023b5fde24e06618e19
Renan Dias <renansdiaz@gmail.com>
2015-05-30 23:05:30 -0300

    tests debit with insufficient funds

--- src/main/java/ca/uwo/csd/cs2212/jshantz/TestBankAccount.java
+++ src/main/java/ca/uwo/csd/cs2212/jshantz/TestBankAccount.java
@@ -1,4 +1,5 @@
 package ca.uwo.csd.cs2212.USERNAME; 
+
 import junit.framework.Assert; 
 import org.junit.Before; 
 import org.junit.Test; 
@@ -10,5 +11,12 @@ public class TestBankAccount {
 		BankAccount account = new BankAccount(10);
 		double amount = account.debit(5);
 		Assert.assertEquals(5.0, amount);
+	}
+
+	@Test
+	public void testDebitWithInsufficientFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(11);
+		Assert.assertEquals(10.0, amount);
 	}
 }
